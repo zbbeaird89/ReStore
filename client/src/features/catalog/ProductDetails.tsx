ad6317ac8e3ca64a23293e57c63a6cc1cd6c1d3c
Zach <zbbeaird1989@gmail.com>
2023-07-22 06:59:34 -0500

    Half of Error Handling module

--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -1,8 +1,8 @@
 import { Divider, Grid, Table, TableBody, TableCell, TableContainer, TableRow, Typography } from "@mui/material";
-import axios from "axios";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { Product } from "../../app/models/product";
+import agent from "../../app/api/agent";
 
 export default function ProductDetails() {
   const {id} = useParams<{id: string}>();
@@ -10,8 +10,8 @@ export default function ProductDetails() {
   const [loading, setLoading] = useState(true);
   
   useEffect(() => {
-    axios.get(`http://localhost:5000/api/products/${id}`)
-      .then(response => setProduct(response.data))
+    id && agent.Catalog.details(parseInt(id))
+      .then(response => setProduct(response))
       .catch(error => console.log(error))
       .finally(() => setLoading(false));
   }, [id])
